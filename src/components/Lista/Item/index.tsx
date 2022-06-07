d68069371709197a1f815ab5a50d594373239e9c
Juliana Vaz da Silva <vazjuliana1993@gmail.com>
2022-06-07 20:24:13 -0300

    projeto react

---
+++ src/components/Lista/Item/index.tsx
@@ -0,0 +1,24 @@
+import style from '../Lista.module.scss';
+import {ITarefa} from '../../../types/tarefa';
+
+interface Props extends ITarefa{
+    selecionaTarefa: (tarefaSelecionada: ITarefa) => void
+}
+
+export default function Item({tarefa, tempo, selecionado, completado, id,  selecionaTarefa}: Props) {
+    return( 
+        <li 
+        className={`${style.item}${selecionado ? style.itemSelecionado : ''}`} 
+        onClick={()=> selecionaTarefa(
+        {
+            tarefa,
+            tempo,
+            selecionado,
+            completado,
+            id
+        })}>
+            <h3> {tarefa} </h3>
+            <span> {tempo} </span>
+        </li>
+    )
+}
---
+++ src/components/Lista/index.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { ITarefa } from '../../types/tarefa';
+import Item from './Item';
+import style from './Lista.module.scss';
+
+interface Props{
+  tarefa: ITarefa[],
+  selecionaTarefa:(tarefaSelecionada: ITarefa) => void
+}
+
+function Lista({ tarefas, selecionaTarefa }: Props }) {
+  return (
+    <aside className={style.listaTarefas}>
+      <h2> Estudos do dia </h2>
+      <ul>
+        {tarefas.map((item => (
+          <Item
+            selecionaTarefa={selecionaTarefa}
+            key={index}
+            {...item}
+          />
+        ))}
+      </ul>
+    </aside>
+  )
+}
+
+export default Lista;
